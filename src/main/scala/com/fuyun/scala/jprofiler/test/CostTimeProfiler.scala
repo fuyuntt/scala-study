e564f93a8262e74233ac6bfd98fb35a8a4d9dd6f
fuyun <fuyun.v5@gmail.com>
2016-08-04 14:53:54 +0800

    新建时间分析工具

---
+++ src/main/scala/com/fuyun/scala/jprofiler/test/CostTimeProfiler.scala
@@ -0,0 +1,39 @@
+package com.fuyun.scala.jprofiler.test
+
+/**
+  * Created by bl05959 on 2016/8/3.
+  */
+class CostTimeProfiler(points: String*) {
+  private val len = points.size
+  private val checkPoints = Array.ofDim[Long](len - 1)
+  private var index = 0
+  private var preTime = 0L
+
+  def start(): Unit = {
+    preTime = System.nanoTime()
+    index = 0
+  }
+
+  def check(): Unit = {
+    val now = System.nanoTime()
+    checkPoints(index) += now - preTime
+    index += 1
+    preTime = now
+  }
+
+  def statistics(): String = {
+    points.zipWithIndex
+      .flatMap { case (point, i) =>
+        val lit = List(point)
+        if (i < len - 1)
+          List(point, f"(${checkPoints(i).toDouble / 1000000}%6.6f ms)")
+        else
+          lit
+      }
+      .mkString(" ")
+  }
+
+  def reset(): Unit = {
+    (0 until(len - 1)).foreach(checkPoints(_) = 0)
+  }
+}
